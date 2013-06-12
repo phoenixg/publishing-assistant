99307bfc6ff6ecfeb17334a33c5855d4f4896675
phx-hp1000 <306761352@qq.com>
2013-06-12 21:54:47 +0800

    该成最新的pm_config文件

--- application/pm/config/pm_config.php
+++ application/pm/config/pm_config.php
@@ -1,11 +1,13 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
-|--------------------------------------------------------------------------
-| Date Format Constants
-|--------------------------------------------------------------------------
-|
+ * ----------------------------------------------------------------------------
+ * 
+ * Date Format Constants
+ *
+ * ----------------------------------------------------------------------------
  */
-define('SCI_ISSUE_DATE','d F Y');
+
+ define('SCI_ISSUE_DATE','d F Y');
 
 /*
  * ----------------------------------------------------------------------------
@@ -20,11 +22,14 @@ $config['journals'] = array(
     'id' => 0,
     'doi_id' => '10.1126/science',
     'name' => 'Science Magazine',
-
-    'article_xml_root' => (defined('ENVIRONMENT') && ENVIRONMENT == 'development') ?
-      'application/pm/issue_xml/VOLUME_NUM/ISSUE_FULL_MONTH/ISSUE_DAY ISSUE_FULL_MONTH--ISSUE_NUM/' 
-      : 'application/pm/issue_xml/VOLUME_NUM/ISSUE_FULL_MONTH/ISSUE_DAY ISSUE_FULL_MONTH--ISSUE_NUM/',
-
+   //'article_xml_root' => '../xml/HIGHWIRE/',
+   
+   
+	  'article_xml_root' => (defined('ENVIRONMENT') && ENVIRONMENT == 'development') ?
+    //  'application/pm/issue_xml/SciTM/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY_ISSUE_NUM/'
+    '../xml/HIGHWIRE/340/ISSUE_FULL_MONTH/ISSUE_DAY ISSUE_FULL_MONTH--ISSUE_NUM/'
+   : '/xml/HIGHWIRE/340/ISSUE_FULL_MONTH/ISSUE_DAY ISSUE_FULL_MONTH--ISSUE_NUM/',
+	
     'publish_day' => 'Friday',
     'data_source' => array(
       array(
@@ -42,15 +47,21 @@ $config['journals'] = array(
     'ext' => ".xml",
     'data_path' => "application/pm/data/issueData.txt",
     'cover_base' => 'http://www.sciencemag.org/content/VOLUME_NUM/ISSUE_NUM/local/cover-enclosure.gif',
-
     'article_base_path' => 'http://www.sciencemag.org/content/VOLUME_NUM/ISSUE_NUM/PAGE.VARIANT',
-
+    'cover_caption' => 'peripherals/covercap.htslp',
   ),
   'sig' => array(
     'id' => 1,
     'doi_id' => '10.1126/scisignal',
     'name' => 'Science Signaling',
-    'article_xml_root' => '/home/highwire/Stke/',
+    //'article_xml_root' => '/home/highwire/Stke/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DATE__ISSUE_NUM/',
+    
+	  'article_xml_root' => (defined('ENVIRONMENT') && ENVIRONMENT == 'development') ?
+    //  'application/pm/issue_xml/SciTM/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY_ISSUE_NUM/'
+    '../xml/HIGHWIRE/Stke/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY__ISSUE_NUM/'
+   : '/home/highwire/Stke/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY__ISSUE_NUM/',
+	
+	
     'publish_day' => 'Tuesday',
     'data_source' => array(
       array(
@@ -61,17 +72,18 @@ $config['journals'] = array(
     'ext' => ".xml",
     'data_path' => "application/pm/data/issueData-sig.txt",
     'cover_base' => 'http://stke.sciencemag.org/content/volVOLUME_NUM/issueISSUE_NUM/cover_toc.gif',
-
-    'article_base_path' => 'http://stke.sciencemag.org/cgi/content/VARIANT/sigtrans;VOLUME_NUM/ISSUE_NUM/PAGE',
 		'article_base_path' => 'http://stke.sciencemag.org/cgi/content/VARIANT/sigtrans;VOLUME_NUM/ISSUE_NUM/PAGE',
-
+    'cover_caption' => 'VOLUME_NUMISSUE_NUM.covercaption.html',
   ),
   'stm' => array(
     'id' => 2,
     'doi_id' => '10.1126/scitranslmed',
     'name' => 'Science Translational Medicine',
-  //  'article_xml_root' => '../xml/SciTM/',
-    'article_xml_root' => '/home/highwire/SciTM/',
+    //'article_xml_root' => '../xml/SciTM/',
+   'article_xml_root' => (defined('ENVIRONMENT') && ENVIRONMENT == 'development') ?
+    //  'application/pm/issue_xml/SciTM/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY_ISSUE_NUM/'
+    '../xml/HIGHWIRE/SciTM/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY_ISSUE_NUM/'
+   : '/home/highwire/SciTM/ISSUE_FULL_YEAR_ISSUE_MONTH_ISSUE_DAY_ISSUE_NUM/',
     'publish_day' => 'Wednesday',
     'data_source' => array(
       array(
@@ -82,27 +94,23 @@ $config['journals'] = array(
     'ext' => ".xml",
     'data_path' => "application/pm/data/issueData-stm.txt",
     'cover_base' => 'http://stm.sciencemag.org/content/VOLUME_NUM/ISSUE_NUM.cover.gif',
-
     'article_base_path' => 'http://stm.sciencemag.org/content/VOLUME_NUM/ISSUE_NUM/PAGE.VARIANT',
-
+    'cover_caption' => 'VOLUME_NUM_ISSUE_NUM.covercaption.html',
   ),
   'news' => array(
     'id' => 3,
     'name' => 'ScienceNOW',
     'publish_day' => 'Today',
   )
-
-
 );
 
-// Points to the next greater access level for an article 
+// Indicates the next VARIANT view state
 $config['next_access_level']  = array(
 	'teaser' => 'abstract',
 	'web-summary' => 'abstract',
 	'excerpt' => 'full',
 	'full' => 'full',
 );
-
 // Points to the next greater access level for an article 
 $config['next_access_level']  = array(
   'teaser' => 'abstract',
@@ -111,7 +119,6 @@ $config['next_access_level']  = array(
   'full' => 'full',
 );
 // Content-display priority definitions
-
 $config['teaser_options'] = array(
 	'A' => array ("teaser"),
 	'B' => array ("teaser", "full"),
@@ -119,14 +126,8 @@ $config['teaser_options'] = array(
 	'D' => array ("web-summary", "excerpt", "full"),
 );
 
-
-//config array for alerts, output and ad placements.
-// The alertMaster uses this array in order to build the list of alerts avalible to the user, and once an alert is chosen
-//to be compiled, it will again use this array to place ads, order the alert, etc
+// include other individual config files
 include 'application/pm/config/sci_alert_config.php';
 include 'application/pm/config/stm_alert_config.php';
 include 'application/pm/config/sig_alert_config.php';
 include 'application/pm/config/news_alert_config.php';
-
-
-
